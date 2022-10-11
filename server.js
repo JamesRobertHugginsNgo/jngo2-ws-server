b7bd069d6488b26e64384ec25cf7c7407b50f080
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2022-10-11 00:22:37 -0400

    Commit code

--- server.js
+++ server.js
@@ -2,103 +2,80 @@ const Ws = require('ws');
 
 const PORT = process.env.PORT || 80;
 
-// const log = console.log.bind(console);
-const log = (...args) => {
-	broadcast(JSON.stringify({
-		type: 'Add Log',
-		args
-	}), true);
-};
-
 const clients = {};
-
 let clientIdCounter = 0;
 
-function broadcast(message, dontLog) {
-	!dontLog && log && log('BROADCAST', message);
-
-	for (const key in clients) {
-		const client = clients[key];
-		client.webSocket.send(message);
+function sendMessage(message) {
+	if (message.to) {
+		clients[message.to].send(JSON.stringify(message));
+	} else {
+		clients.forEach((client) => void client.send(JSON.stringify(message)));
 	}
 }
-
-// ---
-// WEB SOCKET SERVER
-// ---
 
 const server = new Ws.WebSocketServer({ port: PORT });
 
 server.on('connection', (webSocket) => {
-	log && log('SERVER', 'ON CONNECTION');
-
 	const clientId = String(clientIdCounter++);
-	const client = { webSocket };
 
-	webSocket.on('message', (data, isBinary) => {
-		log && log('WEB SOCKET', clientId, 'ON MESSAGE', data.toString('utf8'), isBinary);
-
+	webSocket.on('message', (data) => {
 		try {
 			const message = JSON.parse(data.toString('utf8'));
-			const type = message.type;
+			message.from = clientId;
+			sendMessage(message);
 
-			switch (type) {
-				case 'Set Offer': {
-					const { targetClientId, offer } = message;
-					const newMessage = JSON.stringify({
-						type: 'Set Offer',
-						sourceClientId: clientId, // TODO: Replace with WebRTC peer to peer connection
-						offer
-					});
-
-					clients[targetClientId] && clients[targetClientId].webSocket
-						&& clients[targetClientId].webSocket.send(newMessage);
-
-					break;
-				}
-
-				case 'Set Answer': {
-					const { targetClientId, answer } = message;
-					const newMessage = JSON.stringify({
-						type: 'Set Answer',
-						sourceClientId: clientId, // TODO: Replace with WebRTC peer to peer connection
-						answer
-					});
-
-					clients[targetClientId] && clients[targetClientId].webSocket
-						&& clients[targetClientId].webSocket.send(newMessage);
-
-					break;
-				}
-			}
+			sendMessage({
+				type: 'Log',
+				log: 'Web Socket - On Message',
+				message,
+				clientId
+			});
 		} catch (error) {
-			log && log('WEB SOCKET', clientId, 'ON MESSAGE', error, error);
+			sendMessage({
+				type: 'Log',
+				log: 'Web Socket - On Message - Error',
+				error,
+				clientId
+			});
 		}
 	});
 
 	webSocket.on('close', (code, reason) => {
-		log && log('WEB SOCKET', clientId, 'ON CLOSE', code,  reason.toString('utf8'));
-
 		delete clients[clientId];
+		sendMessage({
+			type: 'Remove',
+			from: clientId
+		});
 
-		broadcast(JSON.stringify({
-			type: 'Remove Client',
+		sendMessage({
+			type: 'Log',
+			log: 'Web Socket - On Close',
+			code,
+			reason,
 			clientId
-		}));
+		});
 	});
 
-	broadcast(JSON.stringify({
-		type: 'Add Client',
-		clientId
-	}));
+	sendMessage({
+		type: 'Add',
+		from: clientId
+	});
 
-	clients[clientId] = client;
+	clients[clientId] = { webSocket };
+
+	sendMessage({
+		type: 'Log',
+		log: 'Server - On Connection'
+	});
 });
 
 server.on('close', () => {
-	log && log('SERVER', 'ON CLOSE');
-
 	for (const key in clients) {
 		delete clients[key];
 	}
+
+	sendMessage({
+		type: 'Log',
+		log: 'Server - On Close'
+	});
 });
